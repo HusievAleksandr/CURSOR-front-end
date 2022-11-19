c877d0c37ff642408fc3dee179b16b871294062f
Husiev Aleksandr <sania7.sg@gmail.com>
2022-11-19 22:50:48 +0200

    Obnovlin

--- js/fobonaci.js
+++ js/fobonaci.js
@@ -1,3 +1,9 @@
 for (let rezult, fobonaci_one= 0, fobonaci_two= 1; fobonaci_two<34; fobonaci_one = fobonaci_two, fobonaci_two = rezult ){
     rezult =fobonaci_one+fobonaci_two;
     console.log(rezult);}
+
+
+
+
+    
+      
