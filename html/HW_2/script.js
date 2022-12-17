5c7e15af43a6bd8e26afaab1e8dbf2606351f48e
Husiev Aleksandr <sania7.sg@gmail.com>
2022-12-17 20:38:26 +0200

    Need refactoring 17.12.2022

--- html/HW_2/script.js
+++ html/HW_2/script.js
@@ -9,7 +9,7 @@ do { M = Number(prompt("Введіть ціле число, до якого бу
      if (N>=M || !Number.isInteger(M)){alert("Введено не коректне число(не ціле) або присутні букви, перше число більше за друге. Введіть коректні дані");}    
 } while (N>=M || !Number.isInteger(M));
 
-double_numbers= confirm("Щоб пропускати парні числа тисни OK??");
+double_numbers= confirm("Щоб пропускати парні числа тисни OK");
      
 for (let i= N ; i<(M+1) ; i++ ){
           if (double_numbers){i%2?rezult += i: rezult;}
