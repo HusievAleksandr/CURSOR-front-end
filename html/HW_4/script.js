b0ffd298f839d7ff1fd6aac96f86c5ad9159279f
Husiev Aleksandr <sania7.sg@gmail.com>
2022-12-17 23:22:07 +0200

    HW_4 17.12.2022

---
+++ html/HW_4/script.js
@@ -0,0 +1,62 @@
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми іструктури даних"];
+const marks = [4, 5, 5, 3, 4, 5];
+
+const pair_up = boy_girl(students);
+const pairs_and_projects = random_command(pair_up, themes);
+const student_mark = grades_students(students, marks);
+const random_assessment =generate_random_estimates(pairs_and_projects);
+
+
+
+function boy_girl (arr){
+        let result =[[],[]];
+        arr.map((el) => el.slice(-1)=='а'? result[0].push(el):result[1].push(el));
+        return result;   }
+
+function random_command(student,them){
+        let result =[];
+        for (let i=0; i<them.length; i++){
+          result.push([student[0][i] + ' і ' + student[1][i], them[i]]) ; 
+        }
+         return result;                }
+
+function grades_students (subscriber, grades){
+        let result =[];
+        for (let i=0; i<students.length; i++){
+          result.push([subscriber[i], grades[i]]) ; 
+        }
+         return result;                      }
+
+function generate_random_estimates (assessment){
+        let result = JSON.parse(JSON.stringify(assessment));
+        for (let el of result){
+        el.push(Math.random().toFixed(0) * (5 - 1) + 1);
+        } return result;
+}
+
+
+const total =`1. Розділ студентів на пари: </br>
+                -  Дівчата: ${pair_up[0]} </br>
+                -  Хлопці: ${pair_up[1]} </br></br>
+              2. Пари та теми проєктів: </br>
+                - ${pairs_and_projects[0]}</br>
+                - ${pairs_and_projects[1]}</br>
+                - ${pairs_and_projects[2]}</br></br>
+              3. Оцінки зі студентом: </br>
+                - ${student_mark[0]}</br>
+                - ${student_mark[1]}</br>
+                - ${student_mark[2]}</br>
+                - ${student_mark[3]}</br>
+                - ${student_mark[4]}</br>
+                - ${student_mark[5]}</br></br>
+              4. Кожній парі випадкову оцінку: </br>
+                - ${random_assessment[0]}</br>
+                - ${random_assessment[1]}</br>
+                - ${random_assessment[2]}</br></br>
+`;
+
+
+
+console.log (pair_up,pairs_and_projects, student_mark, random_assessment);
+document.writeln(total);
