f84bf9622e7f857840ac44823a13a97a462861de
Husiev Aleksandr <sania7.sg@gmail.com>
2022-12-25 22:56:38 +0200

    HW_6 25.12.2022

--- html/HW_6/script.js
+++ html/HW_6/script.js
@@ -27,7 +27,7 @@ const getAverageMark = (students) =>{let result = Object.values(students.subject
 const getStudentInfo = (students) => { let result= {}; Object.assign(result, students); 
                                         result.averageMark = getAverageMark(students);
                                         delete result.subjects;
-                                        const organize = object => ({ course: undefined, ...object })
+                                        const organize = object => ({ course: undefined, name: undefined,averageMark: undefined })
                                         return organize(result);};
 
 
