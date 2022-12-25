ca4734f3169e1def4e54bd8bd15f4f301f523d79
Husiev Aleksandr <sania7.sg@gmail.com>
2022-12-25 22:49:10 +0200

    HW_6 25.12.2022

---
+++ html/HW_6/script.js
@@ -0,0 +1,47 @@
+const students = [
+        {name: "Tanya",
+        course: 3,
+        subjects: {math: [4, 4, 3, 4],
+                   algorithms: [3, 3, 3, 4, 4, 4],
+                   data_science: [5, 5, 3, 4]}
+        }, 
+
+        {name: "Victor",
+        course: 4,
+        subjects: {physics: [5, 5, 5, 3],
+                   economics: [2, 3, 3, 3, 3, 5],
+                   geometry: [5, 5, 2, 3, 5]}
+        }, 
+        
+        {name: "Anton",
+        course: 2,
+        subjects: {statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+                   english: [5, 3],
+                   cosmology: [5, 5, 5, 5]}
+        }               ];
+
+ 
+const getSubjects = (students) => Object.keys(students.subjects).map((el)=> el.replace('_',' ').replace(el[0],el[0].toUpperCase()));
+const getAverageMark = (students) =>{let result = Object.values(students.subjects).reduce((a,b)=> [...a,...b])
+                                        return (result.reduce((a,b)=> a+b)/result.length).toFixed(2);};
+const getStudentInfo = (students) => { let result= {}; Object.assign(result, students); 
+                                        result.averageMark = getAverageMark(students);
+                                        delete result.subjects;
+                                        const organize = object => ({ course: undefined, ...object })
+                                        return organize(result);};
+
+
+
+const hw = `Функція 1: ${getSubjects(students[0])}</br>
+Функція 2: ${getAverageMark(students[0])}</br>
+Функція 3: дв.консоль </br>
+`;
+document.writeln(hw);   
+console.log (hw.replaceAll('</br>',''));
+
+console.warn("Функція 3 :");
+console.log(getStudentInfo(students[0]));
+
+
+
+     
