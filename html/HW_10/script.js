b1ddbb0112abdc983f78a9dc2ff379b04f6d86da
Husiev Aleksandr <sania7.sg@gmail.com>
2023-01-15 00:42:41 +0200

    HW_10 15.01.2023

---
+++ html/HW_10/script.js
@@ -0,0 +1,24 @@
+const get_all_letters = document.querySelectorAll('.musical_letters');
+const keystrokes= document.querySelector('body');
+const music_playback = document.querySelector('audio');
+
+
+keystrokes.addEventListener("click", function(e){
+  let target = e.target;
+  get_all_letters.forEach((el)=>{el.style.border='1px solid silver';});
+ 
+  target.style.border='5px solid rgb(13, 245, 63)';
+  music_playback.setAttribute("src", `audio/${target.textContent}.mp3`);
+                                                });
+
+
+keystrokes.addEventListener("keydown",function(e){
+    get_all_letters.forEach((el)=>{
+    el.style.border='1px solid silver';
+
+    if(el.textContent.toLowerCase()==e.key){
+        el.style.border='5px solid rgb(13, 245, 63)';
+        music_playback.setAttribute("src", `audio/${e.key}.mp3`);}
+                                    });  
+                                                  });
+
