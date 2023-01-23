63152b85c1cd788fb6fd3fd984a8ef98d7095da6
Husiev Aleksandr <sania7.sg@gmail.com>
2023-01-24 00:45:44 +0200

    HW_12 24.01.2023

--- html/HW_12/script.js
+++ html/HW_12/script.js
@@ -2,13 +2,15 @@
 const BASE_URL = "https://swapi.dev/api/";
 const get_people= document.querySelector('body');
 const post_block= document.querySelector('#block');
+const wookieeText = document.querySelector('#WOO');
+
 
 
 
 
 get_people.addEventListener("click", function(e){
     const movie_selection = document.querySelector('#films').value; 
-    const target = e.target; 
+    const target = e.target;
     if(target.defaultValue=='GET'){getDataFromServer(`/films/${movie_selection}/`);}
     if(target.innerText=='NEXT>>'){getDataFromServer('/planets/');}
                                              });
@@ -17,7 +19,7 @@ get_people.addEventListener("click", function(e){
 async function getDataFromServer (selection){
 
     const request = await fetch(`${BASE_URL}${selection}`);
-    const data = await request.json();
+    const data = await request.json(); 
   
     if (selection=='/planets/'){ 
               post_block.replaceChildren();
@@ -28,13 +30,17 @@ async function getDataFromServer (selection){
               data.characters.forEach(async (el)=>{
               const request = await fetch(el);
               const data = await request.json();
-              withdrawDisplayPeople(data.name,data.birth_year,data.gender);
+
+              const request_wookiee = await fetch(`${el}?format=wookiee`);
+              const data_wookiee = await request_wookiee.json();
+              
+              withdrawDisplayPeople(data.name,data.birth_year,data.gender,data_wookiee.whrascwo);
                                         });
           }
                                               }
 
 
-function withdrawDisplayPeople(name,birth_year,gender) {
+function withdrawDisplayPeople(name,birth_year,gender,whrascwo) {
     const people = document.createElement("div");
     const people_p = document.createElement("p");
     people.classList.add('people');
@@ -43,10 +49,10 @@ function withdrawDisplayPeople(name,birth_year,gender) {
     people_images.setAttribute("src",`images/${name}.jpg`)
 
     const people_name = document.createElement('span');
-    people_name.textContent = `Name: ${name}`;
+    people_name.textContent = wookieeText.checked? `Whrascwo: ${whrascwo}`:`Name: ${name}`;
 
     const people_birth_year = document.createElement('span');
-    people_birth_year.textContent = `Birth year: ${birth_year}`;
+    people_birth_year.textContent = wookieeText.checked? `Rhahrcaoac roworarc: ${birth_year}`:`Birth year: ${birth_year}`;
 
     const people_gender =document.createElement('img');
     people_gender.setAttribute("src",`images/${gender=='n/a'|| gender=='none'?'na': gender}.png`);
